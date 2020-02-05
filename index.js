bd1148923282847fa61cf3de5dfef4d9804ee95b
Heath Chiavettone <heath.chiavettone@freenome.com>
2020-02-05 15:02:28 -0800

    Replicated Changes from PR on origin repo

--- index.js
+++ index.js
@@ -3,14 +3,29 @@ var postcss = require('postcss');
 module.exports = postcss.plugin('camelcaser', function camelcaser(options) {
     return function (css) {
         options = options || {};
+        var forceCaseStyle = options.forceCaseStyle || 'lowerCamelCase';
 
         css.walkRules(function (rule) {
-            rule.selector = rule.selector.replace(/(-|_){1,}\w/g,
-            function (match) {
-                return match[match.length - 1].toUpperCase();
-            }).replace(/(\W)[A-Z]/g, function (match) {
-                return match.toLowerCase();
-            });
+            var output = rule.selector.replace(/(-|_){1,}\w/g,
+                function (match) {
+                    return match[match.length - 1].toUpperCase();
+                });
+            switch (forceCaseStyle) {
+                case 'off':
+                    break;
+                case 'UpperCamelCase':
+                    output = output.replace(/(\W)[a-z]/g, function (match) {
+                        return match.toUpperCase();
+                    });
+                    break;
+                case 'lowerCamelCase':
+                default:
+                    output = output.replace(/(\W)[A-Z]/g, function (match) {
+                        return match.toLowerCase();
+                    });
+                    break;
+            }
+            rule.selector = output;
         });
     };
 });
