d93eb1ba37201ad3d4d270c93fa380fff7e1bf0c
Heath Chiavettone <heath.chiavettone@freenome.com>
2020-09-16 14:53:09 -0700

    Migrated postcss-camelcaser to work with Postcss 8 - Followed the steps in this article: https://evilmartians.com/chronicles/postcss-8-plugin-migration

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-var postcss = require('postcss');
+module.exports = function camelcaser(options) {
+    return {
+        postcssPlugin: 'camelcaser',
+        Root(css) {
+            options = options || {};
+            var forceCaseStyle = options.forceCaseStyle || 'lowerCamelCase';
 
-module.exports = postcss.plugin('camelcaser', function camelcaser(options) {
-    return function (css) {
-        options = options || {};
-        var forceCaseStyle = options.forceCaseStyle || 'lowerCamelCase';
-
-        css.walkRules(function (rule) {
-            var output = rule.selector.replace(/(-|_){1,}\w/g,
-                function (match) {
-                    return match[match.length - 1].toUpperCase();
-                });
-            switch (forceCaseStyle) {
-            case 'off':
-                break;
-            case 'UpperCamelCase':
-                output = output.replace(/(\W)[a-z]/g, function (match) {
-                    return match.toUpperCase();
-                });
-                break;
-            case 'lowerCamelCase':
-            default:
-                output = output.replace(/(\W)[A-Z]/g, function (match) {
-                    return match.toLowerCase();
-                });
-                break;
-            }
-            rule.selector = output;
-        });
+            css.walkRules(function (rule) {
+                var output = rule.selector.replace(/(-|_){1,}\w/g,
+                    function (match) {
+                        return match[match.length - 1].toUpperCase();
+                    });
+                switch (forceCaseStyle) {
+                case 'off':
+                    break;
+                case 'UpperCamelCase':
+                    output = output.replace(/(\W)[a-z]/g, function (match) {
+                        return match.toUpperCase();
+                    });
+                    break;
+                case 'lowerCamelCase':
+                default:
+                    output = output.replace(/(\W)[A-Z]/g, function (match) {
+                        return match.toLowerCase();
+                    });
+                    break;
+                }
+                rule.selector = output;
+            });
+        }
     };
-});
+};
+
+module.exports.postcss = true;
